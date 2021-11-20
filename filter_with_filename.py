d1f16b1982e298ef761c06f0e4100364a894d6ff
Шульгина Ирина <fredpery22@gmail.com>
2021-11-20 23:53:14 +0500

    добавление тестового изображения

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -4,6 +4,22 @@ from itertools import product
 
 
 def get_middle(i, j):
+    """Возвращает среднее значение для пикселя(i, j) размера sizeX,sizeY
+
+       :param i: номер строки
+       :type i: int
+       :param j: номер столбца
+       :type j: int
+
+       :rtype: float
+       :return: среднее значение
+       >>>:get_middle(14,5)
+       19.0
+       >>>:get_middle(100,209)
+       182.0
+       >>>:get_middle(5,70)
+       20.0
+       """
     s = 0
     for n, n1 in product(range(i, i + sizeX), range(j, j + sizeY)):
         for a in range(0, len(matrix[int(n)][int(n1)])):
@@ -14,6 +30,14 @@ def get_middle(i, j):
 
 
 def get_color(i, j):
+    """Заполняет матричное представление изображения размера sizeX,sizeY числовыми значениями цветов
+        с градацией серого gradation для соседей пикселя
+
+        :param i: номер строки
+        :type i: int
+        :param j: номер столбца
+        :type j: int
+        """
     s = get_middle(i, j)
     step = 255 / gradation
     for n, n1 in product(range(i, i + sizeX), range(j, j + sizeY)):
@@ -24,6 +48,14 @@ def get_color(i, j):
 
 
 def get_mosaic():
+    """Заполняет матричное представление изображения размера sizeX,sizeY числовыми значениями цветов
+        с градацией серого gradation
+
+        :param i: номер строки
+        :type i: int
+        :param j: номер столбца
+        :type j: int
+        """
     [[get_color(i, j) for j in range(0, y, sizeY)] for i in range(0, x, sizeX)]
     res = Image.fromarray(matrix)
     res.save(newImg)
